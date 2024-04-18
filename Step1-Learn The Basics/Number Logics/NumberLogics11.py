7e8a4d7b524ffae39ec010b6addac3a4b2faced8
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-04-18 12:24:36 +0530

    More NumberLogics Programs (D8)

--- Step1-Learn The Basics/Number Logics/NumberLogics11.py
+++ Step1-Learn The Basics/Number Logics/NumberLogics11.py
@@ -1,25 +1,14 @@
 # Write a Program to calculate prime Factors of number.Write a Program to calculate prime Factors of number.
 
-def prime_factors(num):
-    found_factor = False  # Flag to check if any factor was found
-    for i in range(2, num):
-        if num % i == 0:  # Check if i is a factor of num
-            print(i,end=' ')
-            num = num // i  # Update num by dividing with i
-            found_factor = True
-            break  # Break out of the loop after finding one factor
-    
-    if not found_factor:
-        # If no factor was found in the loop, num is prime itself
-        print(num,end=' ')
+def primefact(n):
+    pnlist = []
+    i = 2
+    while i <= n:
+        if n % i == 0 :
+           pnlist.append(i)
+           n = n/i
+        else :
+            i+=1
+    return pnlist               
 
-        # Optional: Stop recursion if num is prime (base case)
-        return
-    
-    # Recursively find prime factors of the updated num
-    prime_factors(num)
-
-# Example usage
-number = int(input("Enter a number: "))
-print("Prime factors:",end=' ')
-prime_factors(number)
+print(primefact(630))
