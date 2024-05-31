217ef905a4119faa479f1678a12a8ff08e0f626a
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-05-31 15:24:08 +0530

    Single Number III

---
+++ Arrays/ArrayProblems37.py
@@ -0,0 +1,50 @@
+# 260. Single Number III
+# Solved
+# Medium
+# Topics
+# Companies
+# Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+
+# You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,1,3,2,5]
+# Output: [3,5]
+# Explanation:  [5, 3] is also a valid answer.
+# Example 2:
+
+# Input: nums = [-1,0]
+# Output: [-1,0]
+# Example 3:
+
+# Input: nums = [0,1]
+# Output: [1,0]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 3 * 104
+# -231 <= nums[i] <= 231 - 1
+# Each integer in nums will appear twice, only two integers will appear once.
+
+def singleNumber(nums):
+    hash = dict() 
+
+    for i in nums:
+        if i in hash:
+            hash[i] +=1
+        else:
+            hash[i] = 1
+
+    ans = []
+
+    for i,c in hash.items():
+        if c == 1:
+            ans.append(i)
+    return ans                                       
+
+arr = [1,1,2,2,3,4]
+print(singleNumber(arr))
