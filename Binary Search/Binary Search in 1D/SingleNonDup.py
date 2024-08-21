fd93c5e8b790d35a671a40a9aa7e180d9a95c68d
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-08-21 19:55:45 +0530

    Single Element Which is Not Duplicate

---
+++ Binary Search/Binary Search in 1D/SingleNonDup.py
@@ -0,0 +1,43 @@
+# You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+# Return the single element that appears only once.
+
+# Your solution must run in O(log n) time and O(1) space.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,2,3,3,4,4,8,8]
+# Output: 2
+# Example 2:
+
+# Input: nums = [3,3,7,7,10,11,11]
+# Output: 10
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# 0 <= nums[i] <= 105
+
+
+def SingleNonDuplicate(arr):
+    i = 0
+    j = len(arr) - 1
+    
+    while i < j:
+        m = (i + j) // 2
+        
+        if m % 2 == 1:
+            m -= 1
+        
+        if arr[m] == arr[m + 1]:
+            i = m + 2
+        else:
+            j = m
+    
+    return arr[i]
+
+# Test case
+print(SingleNonDuplicate([1, 1, 4, 3, 3, 4, 4, 8, 8]))  # Output: 4
