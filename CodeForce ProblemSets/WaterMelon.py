ca3e1558ed00647d95ae855b28fcc61a98c332f3
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-10-11 11:33:46 +0530

    Update

--- CodeForce ProblemSets/WaterMelon.py
+++ CodeForce ProblemSets/WaterMelon.py
@@ -29,10 +29,3 @@ def Div_Melon(x):
     
 w = int(input())
 Div_Melon(w)    
-
-# w = int(input())
-# if w >= 1 and w<=100:
-#     if w%2 == 0 and w !=2:
-#         print("YES")
-#     else:
-#         print("NO")
