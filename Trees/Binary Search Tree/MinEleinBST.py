f6ce337455a0a9a8cc04fd942e57022e171cc5da
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-11-11 19:46:29 +0530

    ..

--- Trees/Binary Search Tree/MinEleinBST.py
+++ Trees/Binary Search Tree/MinEleinBST.py
@@ -19,18 +19,15 @@ class Node:
         self.left = None
 
 class Solution:
-    # Function to find the minimum element in the given BST.
+
     def minValue(self, root):
         current = root
-        # Traverse to the leftmost node
         while current and current.left:
             current = current.left
         return current.data if current else None
 
-# Initialize the Solution
 sol = Solution()
 
-# Create the BST as per the example
 root = Node(5)
 root.left = Node(4)
 root.right = Node(6)
@@ -38,7 +35,6 @@ root.left.left = Node(3)
 root.right.right = Node(7)
 root.left.left.left = Node(1)
 
-# Find the minimum value in the BST
 print("Minimum value in the BST:", sol.minValue(root))
       
         
