5ace20c9a4d65d579aa3f0dce1c28442d344ced9
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-11-18 20:06:29 +0530

    ..

--- Trees/Binary Trees/BalancedBinaryTree.py
+++ Trees/Binary Trees/BalancedBinaryTree.py
@@ -9,15 +9,6 @@
 
 # Input: root = [1,2,2,3,3,null,null,4,4]
 # Output: false
-# Example 3:
-
-# Input: root = []
-# Output: true
-
-# Constraints:
-
-# The number of nodes in the tree is in the range [0, 5000].
-# -104 <= Node.val <= 104
 class Node:
     def __init__(self, data):
         self.data = data
