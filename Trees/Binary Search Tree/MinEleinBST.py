e5ce7eef60dfcaad1d5a2d3d63e2a9134c4090f7
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-11-21 17:10:30 +0530

    ..

--- Trees/Binary Search Tree/MinEleinBST.py
+++ Trees/Binary Search Tree/MinEleinBST.py
@@ -10,31 +10,26 @@
 #       3          7
 #      /
 #     1
-# # Output: 1
+# Output: 1
 
 class Node:
     def __init__(self, val):
         self.right = None
         self.data = val
         self.left = None
-
 class Solution:
-
     def minValue(self, root):
         current = root
         while current and current.left:
             current = current.left
         return current.data if current else None
-
 sol = Solution()
-
 root = Node(5)
 root.left = Node(4)
 root.right = Node(6)
 root.left.left = Node(3)
 root.right.right = Node(7)
 root.left.left.left = Node(1)
-
 print("Minimum value in the BST:", sol.minValue(root))
       
         
