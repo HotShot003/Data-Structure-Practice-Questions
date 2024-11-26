e558009b574ba0d2b8176c34504f06153d533ab3
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-11-26 22:02:19 +0530

    ..

--- Stack&Queue/Implement QueueUsingStack.py
+++ Stack&Queue/Implement QueueUsingStack.py
@@ -20,15 +20,6 @@
 # Output
 # [null, null, null, 1, 1, false]
 
-# Explanation
-# MyQueue myQueue = new MyQueue();
-# myQueue.push(1); // queue is: [1]
-# myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
-# myQueue.peek(); // return 1
-# myQueue.pop(); // return 1, queue is [2]
-# myQueue.empty(); // return false
- 
- 
 class MyQueue:
     
     def __init__(s):
