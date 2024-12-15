57289dfb8e8d204bb17397eab2d3977bc2904017
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-12-15 22:50:25 +0530

    Comment Added

--- Practice/2LargestNum.py
+++ Practice/2LargestNum.py
@@ -7,7 +7,7 @@ def Largest2Sum(n):
     for i in n:
         if i >= maxnum:
             maxnum = i
-    maxnum-=1        
+    maxnum-=1       # Reason Bcoz here we have accending order (eg=1,1,2,2,3,3,4,5,6,7,7) input if we find max ele then -1 that we get second hightest ele 
     c = 0
     for k in n :
         if k == maxnum:
